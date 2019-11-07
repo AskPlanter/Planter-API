206ec6f3e0b2baeec9814fdf1e06619150862f1c
Jameel Jiwani <jameeljiwani@gmail.com>
2019-11-07 12:01:25 -0500

    changed to post

--- routes/suggestion.js
+++ routes/suggestion.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.post('/', function(req, res, next) {
     // Fallback
     let msg = "I can't seem to find a dispensary in your area!";
     //let city = event.CurrentInput;
