7bfcd7d5fd1965f42cafa780b497d5437a7bc84a
Jameel Jiwani <jameeljiwani@gmail.com>
2019-11-18 01:56:08 -0500

    added route for age verification

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var suggestionRouter = require('./routes/suggestion');
 var findDispensaryRouter = require('./routes/find_dispensary');
+var ageVerificationRouter = require('./routes/age_verification');
 
 var app = express();
 
@@ -27,5 +28,6 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/suggestion', suggestionRouter);
 app.use('/find_dispensary', findDispensaryRouter);
+app.use('/age_verification', ageVerificationRouter);
 
 module.exports = app;
