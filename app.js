fa59918e2f8897d610dd9ef4224d3cf8ecdb5077
Jameel Jiwani <jameeljiwani@gmail.com>
2019-11-18 01:46:24 -0500

    added find dispensary to app.js

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var sassMiddleware = require('node-sass-middleware');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var suggestionRouter = require('./routes/suggestion');
+var findDispensaryRouter = require('./routes/find_dispensary');
 
 var app = express();
 
@@ -25,5 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/suggestion', suggestionRouter);
+app.use('/find_dispensary', findDispensaryRouter);
 
 module.exports = app;
